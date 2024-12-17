e1210c866dccfc82674acec1d6e7124620a0700a
hosein <hoseinbygomjany@gmail.com>
2024-12-17 10:49:02 +0330

    HB-Admin

---
+++ Pirast.Core/Entity/Admins.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirast.Core.Entity
+{
+    public class Admins:ApplicationUser
+    {
+        public string Role { get; set; }
+        public List<Ticket> Tickets { get; set; }
+        public List<Review> Reviews { get; set; }
+        public List<Report> Reports { get; set; }
+        public List<Users> Users { get; set; }
+        public List<Barbershop> Barbershops { get; set; }
+    }
+}
---
+++ Pirast.Core/Entity/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirast.Core.Entity
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string? Guid { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
