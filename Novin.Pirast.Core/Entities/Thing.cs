7f838b75f87ba046aefe46234dd77d68549f7035
babak babaie <babak.ares23@gmail.com>
2024-12-21 23:41:05 +0330

    ORIGINAL

---
+++ Novin.Pirast.Core/Entities/Thing.cs
@@ -0,0 +1,17 @@
+﻿using Novin.Pirast.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novin.Pirast.Core.Entities
+{
+    internal class Thing : IThing
+    {
+        public int Id { get ; set ; }
+        public string Guid { get  ; set; }
+        public DateTime CreationTime { get ; set ; }
+        public DateTime LastModificationTime { get ; set; }
+    }
+}
---
+++ Novin.Pirast.Core/Interfaces/IThing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novin.Pirast.Core.Interfaces
+{
+    public interface IThing
+    {
+        int Id { get; set; }
+        string Guid { get; set; }
+        DateTime CreationTime { get; set; }
+        DateTime LastModificationTime { get; set; }
+    }
+}
