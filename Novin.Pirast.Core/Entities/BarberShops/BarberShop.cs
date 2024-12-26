6a5278c7945416494a0357faba00ac90feeff51d
hosein <hoseinbygomjany@gmail.com>
2024-12-26 14:32:16 +0330

    BarberShops

--- Novin.Pirast.Core/Entities/BarberShops/BarberShop.cs
+++ Novin.Pirast.Core/Entities/BarberShops/BarberShop.cs
@@ -9,42 +9,43 @@ using static Novin.Pirast.Core.Entities.BarberShops.BarberShopService;
 
 namespace Novin.Pirast.Core.Entities.BarberShops
 {
-    public class BarberShop:BarberShopService
+    public class BarberShop : BarberShopService
     {
         //این بخش برای نام اریشگاه - نام مسئول-آدرس- شماره تماس هستش
         #region
-        public  string TitleBarberShop { get; set; }
-        public  string OwnerFirstname { get; set; }
-        public  string OwnerLastname { get; set; }
-        public  string Address { get; set; }
-        [Phone]
-        public PhoneAttribute? ContactNumber { get; set; }
+        public string? TitleBarberShop { get; set; }
+        public string? OwnerFirstname { get; set; }
+        public string? OwnerLastname { get; set; }
+        public string? Address { get; set; }
+        public string? ContactNumber { get; set; }
         public string? Email { get; set; }
         public string? Gender { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; }
         public byte[] Photo { get; set; }
         public DateTime Dateofbirth { get; set; }
-        public string Nationalcode { get; set; }
-        public string CertificateList { get; set; }
+        public string? Nationalcode { get; set; }
+        public string? CertificateList { get; set; }
 
         #endregion
 
         //این برای بخش های مشترک است
         #region 
-        //public <NormalUser> NormalUsers { get; set; }
-        //public <Barber> Barbers { get; set; }
-        //public < Tiket > Tikets { get; set; }
-        //public < Reporting > Reportings { get; set; }
-        //public < Comments > Comments { get; set; }
-        //public < Reservation > Reservations { get; set; }
+        public <NormalUser> NormalUsers { get; set; }
+        public int NormalUsersId { get; set; }
+        public <Barber> Barber { get; set; }
+        public int BarberId { get; set; }
+        public < Tiket > Tikets { get; set; }
+        public int TiketsId { get; set; }
+
+        public < Reporting > Reportings { get; set; }
+        public int ReportingsId { get; set; }
+        public < Comments > Comments { get; set; }
+        public int CommentsId { get; set; }
+
+        public < Reservation > Reservations { get; set; }
+        public int ReservationsId { get; set; }
+
         #endregion
-
-
-
-
-// مجموعه‌ای از سرویس‌هایی که به این دسته‌بندی تعلق دارند.
-//ICollection از پیش در dotnet تعریف شده که برای (Add-Remove-Contains-Count- Clear)
-public ICollection<BarberShopService> Services { get; set; } = new List<BarberShopService>();
 
     }
 }
